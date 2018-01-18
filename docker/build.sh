80930fe9c13211c47d516c7d72ceeb792c0b5969
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2018-01-18 21:37:10 +0100

    docker push

--- docker/build.sh
+++ docker/build.sh
@@ -5,4 +5,4 @@ mvn clean install -Dmaven.test.skip
 cp target/sleepingPillCore-1.0.TDC-jar-with-dependencies.jar docker/sleepingPillCore.jar
 docker build -t trondheimdc/sleepingpillcore docker
 rm docker/sleepingPillCore.jar
-#docker push trondheimdc/sleepingpillcore
+docker push trondheimdc/sleepingpillcore
